3a3286446d2b0d53f4fa77613e1666525f03768a
Saul Field <saul.field@gmail.com>
2025-02-10 20:10:02 -0500

    Bottom-up enumerative synthesis of NOT, AND, OR, and XOR gates

--- synthesis/examples/nand.py
+++ synthesis/examples/nand.py
@@ -1,4 +1,6 @@
 # %%
+from itertools import product
+import time
 from pydantic.dataclasses import dataclass
 
 
@@ -25,6 +27,16 @@ class Input(Node):
 class Nand(Node):
     a: Node
     b: Node
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, Nand):
+            case_1 = self.a == value.a and self.b == value.b
+            case_2 = self.a == value.b and self.b == value.a
+            return case_1 or case_2
+        return super().__eq__(value)
+
+    def __hash__(self) -> int:
+        return super().__hash__()
 
     def __repr__(self) -> str:
         return f"Nand({self.a}, {self.b})"
@@ -81,15 +93,15 @@ LEVELS = [
             ([1, 1], [0]),
         ],
     ),
-    Spec(
-        "HalfAdder",
-        [
-            ([0, 0], [0, 0]),
-            ([0, 1], [0, 1]),
-            ([1, 0], [0, 1]),
-            ([1, 1], [1, 0]),
-        ],
-    ),
+    # Spec(
+    #     "HalfAdder",
+    #     [
+    #         ([0, 0], [0, 0]),
+    #         ([0, 1], [0, 1]),
+    #         ([1, 0], [0, 1]),
+    #         ([1, 1], [1, 0]),
+    #     ],
+    # ),
 ]
 
 
@@ -122,22 +134,53 @@ def check(spec: Spec, program: Program) -> bool:
     return True
 
 
-def main():
-    spec = LEVELS[0]
-    tt = spec.truth_table
-    n_inputs = len(tt[0][0])
-    # n_outputs = len(tt[0][1])
-    inputs = [Input(i) for i in range(n_inputs)]
-    program = Program((Output(Nand(inputs[0], inputs[0])),))
+def gen_programs(n_inputs: int, max_depth: int) -> dict[int, set[Node]]:
+    prog_sets: dict[int, set[Node]] = {0: {Input(i) for i in range(n_inputs)}}
+    for depth in range(1, max_depth + 1):
+        prev = set()
+        for progs in prog_sets.values():
+            prev.update(progs)
+        prog_sets[depth] = {Nand(a, b) for a, b in product(prev, prev)}
+    return prog_sets
 
-    print(f"Component: {spec.name}")
-    for inputs, outputs in spec.truth_table:
-        print(f"{inputs} -> {outputs}")
-    print(f"Program: {program}")
-    for inputs, outputs in tt:
-        result = eval_program(program, inputs)
-        print(f"{inputs} -> {result}")
-    print(f"Spec passed: {check(spec, program)}")
+
+def main():
+    for spec in LEVELS:
+        # spec = LEVELS[1]
+        print(f"Component: {spec.name}")
+        # for inputs, outputs in spec.truth_table:
+        #     print(f"{inputs} -> {outputs}")
+        # print()
+
+        start = time.perf_counter()
+        max_depth = 3
+        n_inputs = len(spec.truth_table[0][0])
+        prog_sets = gen_programs(n_inputs, max_depth)
+        gen_elapsed = time.perf_counter() - start
+
+        start = time.perf_counter()
+        found = False
+        for depth, programs in prog_sets.items():
+            if found:
+                break
+            print(f"Checking {len(programs)} programs at depth {depth}...")
+            for program in programs:
+                full_prog = Program((Output(program),))
+                if check(spec, full_prog):
+                    found = True
+                    print("Program found!")
+                    print(program)
+                    # for inputs, outputs in spec.truth_table:
+                    #     result = eval_program(full_prog, inputs)
+                    #     print(f"{inputs} -> {result}")
+                    break
+        if not found:
+            print("No solution found.")
+        eval_elapsed = time.perf_counter() - start
+
+        print(f"Gen (depth={max_depth}) took: {gen_elapsed:0.2f}s")
+        print(f"Eval took: {eval_elapsed:0.2f}s")
+        print()
 
 
 if __name__ == "__main__":
